3871d5c14b5eafe23f993ee46a5b4c9ea1e266bb
andrewbolotsky <andrew04tlt@gmail.com>
2025-03-19 20:42:09 +0300

    add commands to commands.py

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -1,4 +1,6 @@
-from .wc_command import WcCommand
+from src.commands.pwd_command import PwdCommand
+from src.commands.wc_command import WcCommand
+from src.commands.cat_command import CatCommand
 
 commands = {
     "echo": EchoCommand(),
