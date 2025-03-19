5d7b96c12b402cab077f65bfb1b580838c7d0188
maksimkaSH <shteinmaks@gmail.com>
2025-03-19 19:42:43 +0300

    feat: add command parsing

--- src/cli_engine.py
+++ src/cli_engine.py
@@ -1,6 +1,14 @@
+import shlex
+
+
 class CliEngine:
     def __init__(self):
-        pass
+        self.is_exit = False
+
+    def __parse_command(self, command_line: str):
+        return shlex.split(command_line)
 
     def run(self):
-        pass
+        while not self.is_exit:
+            input_command = input("> ")
+            parsed_command = self.__parse_command(input_command)
