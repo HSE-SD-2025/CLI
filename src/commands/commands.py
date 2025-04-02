b0dfcc981e71bf5543ae323be8f3ab6b41b03f49
maksimkaSH <shteinmaks@gmail.com>
2025-04-02 17:44:44 +0300

    feat: add grep

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -3,6 +3,7 @@ from src.commands.pwd_command import PwdCommand
 from src.commands.wc_command import WcCommand
 from src.commands.cat_command import CatCommand
 from src.commands.exit_command import ExitCommand
+from src.commands.grep_command import GrepCommand
 
 commands = {
     "echo": EchoCommand(),
@@ -10,4 +11,5 @@ commands = {
     "wc": WcCommand(),
     "pwd": PwdCommand(),
     "exit": ExitCommand(),
+    "grep": GrepCommand(),
 }
